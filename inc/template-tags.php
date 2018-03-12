7b64ff75f47459e962eb0f9483487da27043663e
Allan Cole <allan.cole@automattic.com>
2018-03-12 16:26:02 -0400

    Adding block styling to featured images, post titles and cover images w & w/o text

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,6 +7,41 @@
  * @package Music Theme
  */
 
+if ( ! function_exists( 'musictheme_post_thumbnail' ) ) :
+/**
+ * Displays an optional post thumbnail.
+ *
+ * Wraps the post thumbnail in an anchor element on index views, or a div
+ * element when on single views.
+ */
+function musictheme_post_thumbnail() {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	if ( is_singular() ) :
+	?>
+
+	<figure class="entry-featured-image">
+		<?php the_post_thumbnail(); ?>
+	</figure><!-- .post-thumbnail -->
+
+	<?php else : ?>
+
+	<a class="entry-featured-image" href="<?php the_permalink(); ?>" aria-hidden="true">
+		<?php
+			the_post_thumbnail( 'post-thumbnail', array(
+				'alt' => the_title_attribute( array(
+					'echo' => false,
+				) ),
+			) );
+		?>
+	</a>
+
+	<?php endif; // End is_singular().
+}
+endif;
+
 if ( ! function_exists( 'musictheme_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time and author.
--- template-parts/content.php
+++ template-parts/content.php
@@ -17,8 +17,11 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+		?>
 
-		if ( 'post' === get_post_type() ) : ?>
+		<?php musictheme_post_thumbnail(); ?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php musictheme_posted_on(); ?>
 		</div><!-- .entry-meta -->
