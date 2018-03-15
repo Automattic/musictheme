1b649a870423a3a22e539e35c63ffc68ab25e03a
Allan Cole <allan.cole@automattic.com>
2018-03-15 14:15:43 -0400

    Fixes header color and layout to match updated designs, Show page header title on index

--- index.php
+++ index.php
@@ -17,15 +17,11 @@ get_header(); ?>
 	<main id="primary" class="site-main">
 
 	<?php
-	if ( have_posts() ) :
-
-		if ( is_home() && ! is_front_page() ) : ?>
-			<header>
-				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-			</header>
-
-		<?php
-		endif;
+	if ( have_posts() ) : ?>
+		<header class="page-header">
+			<h1 class="page-title"><?php single_post_title(); ?></h1>
+		</header>
+	<?php
 
 		/* Start the Loop */
 		while ( have_posts() ) : the_post();
