a217566cdd8027ff5fe7a7a40034682a7a98f000
Allan Cole <allan.cole@automattic.com>
2018-03-22 18:45:57 -0400

    Cleaning up post header and comment borders, adds a wrapper to entry-header to set a max-width on larger screens

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,22 +11,24 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		?>
+		<div class="entry-header-wrap">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
+			?>
 
-		<?php musictheme_post_thumbnail(); ?>
+			<?php musictheme_post_thumbnail(); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php musictheme_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+			<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php musictheme_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
