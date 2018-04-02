b6b4b2abdd014b1e65cb74c780194ff735c4c9d9
Allan Cole <allan.cole@automattic.com>
2018-04-02 12:20:57 -0400

    Cleaning up footer text

--- footer.php
+++ footer.php
@@ -13,14 +13,14 @@
 
 <footer id="colophon" class="site-footer">
 	<div class="site-info">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php
+		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'musictheme' ) ); ?>"><?php
 			/* translators: %s: CMS name, i.e. WordPress. */
-			printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
+			printf( esc_html__( 'Proudly powered by %s', 'musictheme' ), 'WordPress' );
 		?></a>
 		<span class="sep"> | </span>
 		<?php
 			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
+			printf( esc_html__( 'Theme: %1$s by %2$s.', 'musictheme' ), 'Music', '<a href="https://automattic.com/">Automattic</a>' );
 		?>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
