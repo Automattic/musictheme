48cc3c205c0066c42d1ab1af92df39f015f65dc6
Kjell Reigstad <kjell@kjellr.com>
2018-04-03 14:48:06 -0400

    Cutting down on code by just pulling in single.php

--- page-templates/hide-header.php
+++ page-templates/hide-header.php
@@ -5,29 +5,6 @@
  * @package Music Theme
  */
 
-get_header(); ?>
-
-	<main id="primary" class="site-main">
-
-	<?php
-	while ( have_posts() ) : the_post();
-
-		get_template_part( 'template-parts/content-single', get_post_type() );
-
-		the_post_navigation( array(
-			'prev_text' => musictheme_get_svg( array( 'icon' => 'previous' ) ) . ' %title',
-			'next_text' => '%title ' . musictheme_get_svg( array( 'icon' => 'next' ) ),
-		) );
-
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) :
-			comments_template();
-		endif;
-
-	endwhile; // End of the loop.
-	?>
-
-	</main><!-- #primary -->
-
-<?php
-get_footer();
+// This template is all handled by CSS, so we're
+// just replicating the single.php template here.
+get_template_part( 'single' );
