faa99ff900dd5c7a66618688e3b46121b414041c
Kjell Reigstad <kjell@kjellr.com>
2018-04-11 08:38:59 -0400

    Giving pages support for featured images.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,6 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php musictheme_post_thumbnail(); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
