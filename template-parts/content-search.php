259b46f6de7d8d856cf1d24b5179facf4b549443
Kjell Reigstad <kjell@kjellr.com>
2018-04-24 15:03:45 -0400

    Making entry-search more like content.
    This standardizes the display of our posts in post streams. Fixes #29.

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,20 +11,29 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php musictheme_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<div class="entry-header-wrap">
+			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+			<?php musictheme_post_thumbnail( 'medium' ); ?>
+		</div>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div class="entry-content">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php musictheme_entry_footer(); ?>
+		<div class="entry-footer-wrap">
+			<?php if ( 'post' === get_post_type() ) : ?>
+			<span class="entry-meta">
+				<?php musictheme_posted_on(); ?>
+			</span><!-- .entry-meta -->
+			<?php endif; ?>
+			<?php musictheme_entry_footer(); ?>
+			<div class="wp-block-button">
+			<a href="<?php the_permalink(); ?>" class="wp-block-button__link">
+				<?php _e( 'Read more', 'musictheme' ); ?>
+			</a>
+		</div>
+		</div>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
