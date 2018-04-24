08191163e6f7d7db425feb93d440d30ef4bc2a41
Kjell Reigstad <kjell@kjellr.com>
2018-04-24 14:49:42 -0400

    Removing widgets from the 404 page.
    We’ll re-evaluate the use of widgets as part of #26.

--- 404.php
+++ 404.php
@@ -17,37 +17,9 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'musictheme' ); ?></p>
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'musictheme' ); ?></p>
 
-				<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-				?>
-
-				<div class="widget widget_categories">
-					<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'musictheme' ); ?></h2>
-					<ul>
-					<?php
-						wp_list_categories( array(
-							'orderby'    => 'count',
-							'order'      => 'DESC',
-							'show_count' => 1,
-							'title_li'   => '',
-							'number'     => 10,
-						) );
-					?>
-					</ul>
-				</div><!-- .widget -->
-
-				<?php
-
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'musictheme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-				?>
+				<?php get_search_form(); ?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
