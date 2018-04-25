938fd1b6d1e4bc07848afac981909baf3a078b9a
Allan Cole <allan.cole@automattic.com>
2018-04-25 14:47:30 -0400

    Fixing infinite scroll issue, See: #27

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -18,6 +18,8 @@ function musictheme_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'primary',
+		'wrapper'   => false,
+		'type'      => 'click',
 		'render'    => 'musictheme_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
