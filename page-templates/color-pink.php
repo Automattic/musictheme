2ca13b45673e7fbe23017f9ff15d30a389e6066c
Kjell Reigstad <kjell@kjellr.com>
2018-04-30 08:24:27 -0400

    Adding page templates + base CSS

---
+++ page-templates/color-pink.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Pink Page Header
+ *
+ * @package Music Theme
+ */
+
+// This template is all handled by CSS, so we're
+// just replicating the page.php template here.
+get_template_part( 'page' );
